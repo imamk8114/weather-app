95242b80555e9faabbd60ce0f9fdefce1b3eed9f
imamk8114 <imamk8114@gmail.com>
2023-04-03 23:14:30 +0530

    check for missing object values

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -4,33 +4,27 @@ import WeatherData from './WeatherData';
 function SearchBar() {
   const [query, setQuery] = useState('');
   const [weatherData, setWeatherData] = useState(null);
+  const apiKey = 'b88860b80aa59ba2546bca339b6fd14a';
 
   async function handleSearch() {
     try {
-      const apiKey = 'b88860b80aa59ba2546bca339b6fd14a';
       const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&units=metric&appid=${apiKey}`;
       const response = await fetch(url);
       const data = await response.json();
 
-      const main = {
-        sky:data.weather.main,
-        icon: data.weather.icon,
-        description: data.weather.description,
-        wind_speed: data.wind.speed,
-        lat:data.coord.lat,
-        lon:data.coord.lon
-      };
-
       const weather ={
-        temp: data.main.temp,
+        lat:data.coord.lat,
+        lon:data.coord.lon,
+        sky:data.weather.main,
+        description: data.weather.description,
         temp_min: data.main.temp_min,
-        feels_like: data.main.feels_like,
         temp_max: data.main.temp_max,
         pressure: data.main.pressure,
-        humidity:data.main.humidity
+        humidity:data.main.humidity,
+        wind_speed: data.wind.speed
       }
 
-      setWeatherData({ city: data.name, main, weather });
+      setWeatherData({ city: data.name, temp:data.main.temp, weather });
     } catch (error) {
       console.error(error);
     }
--- src/WeatherData.js
+++ src/WeatherData.js
@@ -2,21 +2,26 @@ import React from 'react';
 
 function WeatherData(props) {
   const { weatherData } = props;
-
+  console.log(weatherData.weather);
   return (
     <div>
       <h2>{weatherData.city}</h2>
-      <h3>{weatherData.weather.temp} °C</h3>
-      <img
-        alt={weatherData.main.description}
-      />
-      <p>{weatherData.main.description}</p>
-      <div>
-       {weatherData.weather.array.forEach(([key,value]) => {
-            <list>
-                {key} : {value}
-            </list>
-       })}
+      <h3>{weatherData.temp} °C</h3>
+
+      <p>{weatherData.weather.description}</p>
+
+      <div className='weather'>
+       {Object.keys(weatherData.weather).map((key, i) => {
+          if (weatherData.weather[key]) {
+            return (
+              <list className="weather_item" key={i}>
+                {key} : {weatherData.weather[key]}
+              </list>
+            );
+          } else {
+            return null;
+          }
+        })}
       </div>
     </div>
   );
